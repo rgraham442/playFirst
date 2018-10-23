01701648fdd3cec23cd8c903542b48583b64ff37
Robin <robjgraham1996@gmail.com>
2018-10-23 09:01:25 +0100

    Github running again commit

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,4 +9,18 @@ class Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def escape = Action {implicit request: Request[AnyContent] =>
+    Redirect("http://bbc.co.uk")
+  }
+
+  def hello(name: String) = Action {implicit request: Request[AnyContent] =>
+    Ok("Hello " + name + "!")
+  }
+
+  def redirectable = Action {implicit request: Request[AnyContent] =>
+    Redirect(routes.Application.hello("Hugh Mann"))
+  }
+
+
+
 }
